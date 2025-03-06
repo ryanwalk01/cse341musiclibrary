56b41b05e35769171b5d5f933238308ee69864f3
ryanwalk01 <ryan.walk01@gmail.com>
2025-03-06 14:09:53 -0700

    Added basic NodeJS code

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,5 @@ const router = require('express').Router();
 
 router.use('/api-docs', require('./swagger'));
 
-router.use('/towers', require('./towers'));
-
-router.use('/shrines', require('./shrines'));
-
-
 
 module.exports = router;
